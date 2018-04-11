f9c46e5cf2854c4ecc8f416ceeaa807448b95fd6
Wirek90 <34507304+Wirek90@users.noreply.github.com>
2018-04-11 02:50:06 +0200

    added US_FIELDS for view_users template

--- TEST/util.py
+++ TEST/util.py
@@ -5,3 +5,4 @@
 US_FIELDS= ['UserName','Registration Time', 'Reputation']
 QUEST_FIELDS = ['id', 'submission_time', 'view_number', 'vote_number', 'title', 'message', 'image']
 ANS_FIELDS = ['id', 'submission_time', 'vote_number', 'question_id', 'message', 'image']
+ 
